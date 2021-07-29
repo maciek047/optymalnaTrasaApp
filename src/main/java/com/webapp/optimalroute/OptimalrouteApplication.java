c9660cebf07617a99e2a41b1c0aefdc07d3ffa2b
maciek047 <maciek.047@gmail.com>
2021-07-29 21:52:46 +0200

    app startPack

---
+++ src/main/java/com/webapp/optimalroute/OptimalrouteApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.optimalroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimalrouteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptimalrouteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webapp/optimalroute/OptimalrouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.optimalroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimalrouteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
